b86f5909a60ac216378ce668440805a0ff2e7871
Wesley Hotalen <wes.hotalen@gmail.com>
2018-07-28 09:39:22 -0400

    Added all links and images for planets.

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,5 +1,12 @@
 import { Component } from '@angular/core';
 
+export interface Tile {
+  cols: number;
+  rows: number;
+  text: string;
+  title: string;
+}
+
 @Component({
   selector: 'home',
   templateUrl: './home.component.html',
@@ -7,4 +14,19 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
   title = 'Outer Space';
+
+
+  rockyTiles: Tile[] = [
+    {text: 'Let\'s Explore Mercury!', cols: 1, rows: 1, title: 'mercury'},
+    {text: 'Let\'s Explore Venus!', cols: 1, rows: 1, title: 'venus'},
+    {text: 'Let\'s Explore Earth!', cols: 1, rows: 1, title: 'earth'},
+    {text: 'Let\'s Explore Mars!', cols: 1, rows: 1, title: 'mars'},
+  ];
+
+  gasTiles: Tile[] = [
+    {text: 'Let\'s Explore Jupiter!', cols: 1, rows: 1, title: 'jupiter'},
+    {text: 'Let\'s Explore Saturn!', cols: 1, rows: 1, title: 'saturn'},
+    {text: 'Let\'s Explore Uranus!', cols: 1, rows: 1, title: 'uranus'},
+    {text: 'Let\'s Explore Neptune!', cols: 1, rows: 1, title: 'neptune'},
+  ];
 }
