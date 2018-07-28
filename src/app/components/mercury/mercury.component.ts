8b43040b0993ff5f26b0bdbe034c63bc9493f64b
Wesley Hotalen <wes.hotalen@gmail.com>
2018-07-28 09:42:52 -0400

    Added more images for planets

---
+++ src/app/components/mercury/mercury.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  templateUrl: './mercury.component.html',
+  styleUrls: ['./mercury.component.css']
+})
+export class MercuryComponent {
+  title = 'Mercury';
+}
