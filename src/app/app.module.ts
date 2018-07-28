489f5e5eb7e0f42eb30b78f37a6f646a58d19311
Wesley Hotalen <wes.hotalen@gmail.com>
2018-07-27 20:51:51 -0400

    Added Mercury

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,26 +3,30 @@ import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatButtonModule } from '@angular/material/button';
+import { MatGridListModule } from '@angular/material/grid-list';
 import { routing } from './app.routing';
 
 import { AppComponent } from './app.component';
 import { NavComponent} from './components/nav/nav.component';
 import { HomeComponent} from './components/home/home.component';
 import { SunComponent } from './components/sun/sun.component';
+import { MercuryComponent } from './components/mercury/mercury.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
     HomeComponent,
-    SunComponent
+    SunComponent,
+    MercuryComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     routing,
     MatMenuModule,
-    MatButtonModule
+    MatButtonModule,
+    MatGridListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,11 +1,14 @@
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { SunComponent} from './components/sun/sun.component';
+import { MercuryComponent } from './components/mercury/mercury.component';
 
 
 const appRoutes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'sun', component: SunComponent },
+  { path: 'mercury', component: MercuryComponent },
+
   // otherwise redirect to home
   { path: '**', redirectTo: 'home' }
 ];
